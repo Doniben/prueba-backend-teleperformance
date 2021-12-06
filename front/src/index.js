21a8bfe7bae3ddbbe9c2aa436b1edc26e9058f87
doniben <doniben@esperanto.co>
2021-12-06 02:42:54 -0500

    Added toastify

--- front/src/index.js
+++ front/src/index.js
@@ -18,6 +18,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.min.css';
 
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/now-ui-dashboard.scss?v1.5.0";
